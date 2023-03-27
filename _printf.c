8e31c16fb1661539b70ddc9db4433924843ffd00
Air-KS <kevinrogeret@gmail.com>
2023-03-27 12:38:13 +0000

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+/**
+*
+*
+*
+*/
+
+int _printf(const char *format, ...)
+{
+	va_list argumentPrintf;
+	va_start(argumentsPrintf, format);
+
+	int indexPrintf = 0, int Count = 0;
+
+	const char *BrowseToCharacters = format;
+}
