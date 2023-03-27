25e2455bc1bad7ee5c8fc3ac5ce77be320db4416
Air-KS <kevinrogeret@gmail.com>
2023-03-27 12:35:02 +0000

    Prototype

--- main.h
+++ main.h
@@ -1,9 +1,13 @@
 #ifndef PROTOTYPE_H
 #define PROTOTYPE_H
 
-/******** Bibliothèque prototypes **********/
+/******** Bibliothèque **********/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strarg.h>
+
+/******** Function prototypes **********/
+int _printf(const char *format, ...);
 
 #endif
