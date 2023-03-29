01c53d6aa1191686aa5d023aec5cc36d60c6b015
Air-KS <kevinrogeret@gmail.com>
2023-03-29 07:45:49 +0000

    Test files

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <limits.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -7,37 +9,13 @@
  */
 int main(void)
 {
-	char letter;
-	char monString[55] = "Bonjour, je suis la variable String avec 54 caracters";
-	int maVariableChar = 'C';
-	int monInt = '9';
-	double monFloat = 3.14159;
-
-	_printf("Test un première phrase\n");
-	_printf("Test ma premier Varibale Char -> %c\n", maVariableChar);
-	_printf("Ma variable Char doit sortir la lettre C\n\n");
-	_printf("Très bien, faisons une boucle for \"Affichier les lettres de A à Z\"\n\n");
-
-	for (letter = 'A'; letter <= 'Z'; letter++)
-	{
-		putchar(letter);
-	}
-	printf("\n\n");
-
-	_printf("Génial, il faut essayer un string maintenant.\"\n\n");
-
-	_printf("Préparons nos variables\n");
-	_printf("Je Test la variable string -> %s\n", monString);
-
-	_printf("Nice ! Tout semble fonctionner\n");
-
-	_printf("Est-ce que ça s'affiche ?? %d\n\n", monInt);
-
-	_printf("Est-ce que ça s'affiche ?? %f\n\n", monFloat);
-
-	_printf("Affiche 100%\0 \n\n", maVariableChar);
-
-	_printf("Normalement la phrase suivente devrait être une Erreur\n");
-
-	return (0);
+     _printf("Let's try to printf a simple sentence.\n");
+    printf("Let's try to printf a simple sentence.\n");
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+	_printf("Test 100% s'affiche ??\n");
+	_printf("Bonjour % %%\n");
+    return (0);
 }
